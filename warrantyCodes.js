ca912f77cf75ee897aeea22c3730f56c2e507728
imcinnes31 <imcinnes31@gmail.com>
2024-12-10 23:37:45 -0600

    Update warrantyCodes.js

--- warrantyCodes.js
+++ warrantyCodes.js
@@ -580,7 +580,7 @@ const warrantyDict2 = {
 			"iMac M4" : {barcodeNum: 499999535103, code: "SR1A2Z/A", price: 229.99, totalYears: 3},
 			"Mac Mini M4" : {barcodeNum: 499999535110, code: "SR1C2Z/A", price: 229.99, totalYears: 3},
 			"16in MacBook Pro M4" : {barcodeNum: 499999532027, code: "SR192Z/A", price: 529.99, totalYears: 3},
-			"14in MacBook Pro M3" : {barcodeNum: 499999532034, code: "SR182Z/A", price: 379.99, totalYears: 3},
+			"14in MacBook Pro M4" : {barcodeNum: 499999532034, code: "SR182Z/A", price: 379.99, totalYears: 3},
 		},
 		"IPad": {
 			"iPad 9th gen" : {barcodeNum: 194252881774, code: "SCM32Z/A", price: 79.99, totalYears: 2},
